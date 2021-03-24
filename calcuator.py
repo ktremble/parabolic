d26728da196df42ac8c2ece69dac72f19d1bac99
ktremble <k.tremble00@gmail.com>
2021-03-24 12:50:09 -0700

    Added comments

--- calcuator.py
+++ calcuator.py
@@ -1,3 +1,9 @@
+'''
+Some code borrowed/modified from a given simple
+graphics library used for creating a canvas and drawing
+simple shapes
+'''
+
 import tkinter
 from tkinter import *
 import time
@@ -13,13 +19,14 @@ class graphics:
         self.primary.title(title)
         self.primary.geometry('%dx%d+%d+%d' % (w+150, h, 50, 100))
         self.canvas = tkinter.Canvas(self.primary, width=w, height=h, highlightthickness=0)
-        #self.canvas.focus_set()
+        self.canvas.focus_set()
         self.canvas.pack(side=tkinter.RIGHT)
         self.mouse_x = 0
         self.mouse_y = 0
         self.images = {}
         self.frame_count = 0
         self.__handle_motion()
+        self.w = w
 
     '''
     BEGIN PRIVATE FUNCTION(S)
@@ -37,75 +44,107 @@ class graphics:
     END PRIVATE FUNCTION(S)
     '''
 
+    '''
+    START CALCULATOR FUNCTIONS
+    '''
     def input_section(self):
+        '''
+        Creates the data input section for what will
+        be calculated. Based on the formula x=X and y=a*x**2+Y,
+        gets variables, a,X,Y
+        '''
         self.a_val=1
         self.x_val=1
         self.y_val = 1
 
+        # section for getting (a) variable
         self.a_label = tkinter.Label(text="a (float)")
         self.a = tkinter.Entry(justify='center')
         self.a_label.pack()
-        self.a.insert(0,"0.1")
+        self.a.insert(0,"0.01")
         self.a.pack()
 
+        # section for getting (x) variable
         self.x_label = tkinter.Label(text="x-vertex (integer)")
         self.x = tkinter.Entry(justify='center')
         self.x_label.pack()
         self.x.insert(0,"0")
         self.x.pack()
 
+        # section for getting (y) variable
         self.y_label = tkinter.Label(text="y-vertex (integer)")
         self.y = tkinter.Entry(justify='center')
         self.y_label.pack()
         self.y.insert(0,"0")
         self.y.pack()
 
+        # section for getting direction (choice) variable
         self.choice = IntVar()
         self.r1 = Radiobutton(text="left-right", variable=self.choice, value=1)
         self.r2 = Radiobutton(text="up-down", variable=self.choice, value=0)
         self.r1.pack()
         self.r2.pack()
 
+        # Button to click to draw parabola
         self.enter = tkinter.Button(text="Graph Parabola", command=self.get_result)
         self.enter.pack()
 
-
-
-
     def get_result(self):
+        '''
+        Event handler for when "graph parabola" is clicked.
+        Gets the values from the data entry points and calls
+        the function to draw the parabola on the canvas.
+        '''
+        gui = self
+        width = self.w
+
         gui.clear()
         gui.rectangle(0,0,width+10,width+10,'white')
         gui.draw_grid()
 
+        #get value from input
         a_val = self.a.get()
         x_val = self.x.get()
         y_val = self.y.get()
         choice = self.choice.get()
 
-        #print(a_val, x_val, y_val, choice)
+        print(choice)
+        choice = False
         if choice==1:
             choice = True
-        else:
-            choice = False
 
-
-        x = int(x_val)           # X vertex
-        y = int(y_val)           # Y Vertex
-        a = float(a_val)       # up or down
-        swap = choice     # False = Vertical parabola, True = horizontal
+        print(choice)
+        x = int(x_val)      # X vertex
+        y = int(y_val)      # Y Vertex
+        a = float(a_val)    # up or down
+        swap = choice       # False = Vertical parabola, True = horizontal
 
         # draw parabola
         self.parabolic(-a,x,y,swap)
 
 
-
     def parabolic(self,a,X,Y,swap):
+        '''
+        Based on vertex formula for parabolas. Draws a parabola
+        on the canvas witht the given variables. Parabola is drawn
+        Based on the origin of the grid.
+        a: the a constant in the parabola function
+        X: the location of the X vertex of the parabola
+        Y: the location of the Y vertex of the parabola
+        swap: if True the X and Y values are swapped when calculating
+                rotates the parabola sideways
+        '''
+        gui = self
+        width = self.w
+        #Formula for parabolas:
         # y = x**2
         # y = a(x-X)**2+Y
 
-        X = X+width//2
+        X = X+width//2      #X and Y should start at the center of canvas
         Y = -Y+width//2
 
+        # Gets a list of all x_coords and calculates y using the value
+        # Creates a list of x-y coordinate tuples
         if swap:
             x_coords = range(-width, width+1)
             coords = [(x+Y,a*(x)**2+X) for x in x_coords]
@@ -113,6 +152,7 @@ class graphics:
             x_coords = range(-width, width+1,1)
             coords = [(x+X,a*(x)**2+Y) for x in x_coords]
 
+        #Draws a circle at each coordinate tuple
         for circle in coords:
             x2 = circle[0]
             y2 = circle[1]
@@ -122,31 +162,45 @@ class graphics:
             else:
                 gui.ellipse(x2,y2,3,3,color)
 
-        # line of symmetry
+        # Draw line of symmetry and mark vertex
         if swap:
             gui.line(0,Y,width,Y,'purple',2)
         else:
             gui.line(X,0,X,width,'purple',2)
         gui.ellipse(X,Y,10,10,'purple')
-        boo = Y-width//2
-        gui.text(X+5,Y,"({},{})".format(X-width//2,-boo),'purple',12)
+        gui.text(X+5,Y,"({},{})".format(X-width//2,-(Y-width//2)),'purple',12)
 
 
 
     def draw_grid(self):
-        #grid
+        '''
+        Draws the graph grid with the center being the origin and
+        tick marks every 10 steps. Every 50 steps are marked with
+        a number for easy reading.
+        '''
+        gui = self
+        width = self.w
+        scale = 50
+
+        #draw X and Y axis
         gui.line(0,width//2,width,width//2,'blue',2)
         gui.line(width//2,0,width//2,width,'blue',2)
         for mark in range(0,width+1):
+            # uniform number width
             num_x = '{:4d}'.format(mark-width//2)
             num_y = '{:4d}'.format(width//2-mark)
 
+            # If a tick mark is at step 50 make it a little wider than the others
             widen = 5
             if mark%scale==0:
                 widen += 4
+
+            #Draw a tick mark every 10 steps on x and y axis
             if mark%10==0:
                 gui.line(mark, width//2-widen, mark, width//2+widen, 'blue', 2)
                 gui.line(width//2-widen, mark, width//2+widen, mark, 'blue', 2)
+
+            # Draw the number for the step 50 ticks, 0 drawn slightly off to the side
             offset = mark - 13
             if mark-width//2 == 0:
                     gui.text(offset, width//2+3, "0", 'blue', 8)
@@ -154,6 +208,9 @@ class graphics:
                 gui.text(offset, width//2+widen, num_x, 'blue', 8)
                 gui.text(width//2-32,offset+5, num_y, 'blue', 8)
 
+    '''
+    END CALCULATOR FUNCTIONS
+    '''
 
     def resize(self, width, height):
         self.primary.geometry(str(width) + 'x' + str(height))
@@ -165,34 +222,6 @@ class graphics:
         '''
         text = self.canvas.create_text(x, y, text=content, fill=fill, font=('Arial', size), anchor='nw')
         self.canvas.move(text, 0, 0)
-
-    def set_left_click_action(self, callee):
-        ''' Call the callee function whenever the left click happens.
-        callee should take two parameters, the mouse x and mouse y coordinates.
-        '''
-        def left_click_action(event):
-            callee(self, event.x, event.y)
-        ''' <Button-1> is the left-most mouse button '''
-        self.canvas.bind('<Button-1>', left_click_action)
-
-    def set_right_click_action(self, callee):
-        ''' Call the callee function whenever the right click happens.
-        callee should take two parameters, the mouse x and mouse y coordinates.
-        '''
-        def right_click_action(event):
-            callee(self, event.x, event.y)
-        ''' <Button-2> or <Button-3> is the right-most mouse button.
-        Both are set just in case '''
-        self.canvas.bind('<Button-2>', right_click_action)
-        self.canvas.bind('<Button-3>', right_click_action)
-
-    def set_keyboard_action(self, callee):
-        ''' Call the callee function whenever a keyboard key is pressed.
-        callee should take one parameter, a char representing the key.
-        '''
-        def keyboard_action(event):
-            callee(self, event.char)
-        self.canvas.bind('<KeyPress>', keyboard_action)
 
     def get_color_string(self, red, green, blue):
         ''' accepts three ints that should represent and RGB color.
@@ -267,15 +296,18 @@ class graphics:
         '''
         self.canvas.delete('all')
 
-width = 500
-scale = 50
 
-gui = graphics(width,width,"Parabolic Funtime")
-gui.rectangle(0,0,width+10,width+10,'white')
-# grid
-gui.draw_grid()
-gui.input_section()
-gui.parabolic(-.01,0,0,False)
+def main():
+    width = 500
+    scale = 50
 
+    gui = graphics(width,width,"Parabolic Funtime")
+    gui.rectangle(0,0,width+10,width+10,'white')
+    # grid
+    gui.draw_grid()
+    gui.input_section()
+    gui.parabolic(-.01,0,0,False)
+
+main()
 
 
